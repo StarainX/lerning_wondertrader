24d744e825847b95c2ee4b572db69f33fced59aa
StarainX <710783461@qq.com>
2025-04-16 02:15:15 +0800

    加入了进程池处理，不过其实这块的性能瓶颈在磁盘读写速度上，磁盘读写慢了怎么也快不了。

--- code/3.3 全市场提取多进程版.py
+++ code/3.3 全市场提取多进程版.py
@@ -8,7 +8,6 @@ import ctypes
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from tqdm import tqdm
 from colorama import Fore, Style, init
-from functools import partial
 from itertools import islice
 
 # 初始化颜色支持（Windows需要）
@@ -229,7 +228,7 @@ if __name__ == "__main__":
 
     # 操作说明
     print(f"{Fore.WHITE}📌 格式要求：")
-    print(f"1. 源档案目录结构：{Fore.CYAN}根目录/2014/合约文件.zip")
+    print(f"1. 源档案目录结构：{Fore.CYAN}根目录/2014/YYYYMMDD.zip")
     print(f"2. 自动跳过已存在的文件{Fore.YELLOW}（避免重复提取）\n")
 
     # 模式选择
